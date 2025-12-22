db2854b7bc33dfd233922b3f05b29f1417286efe
Dilina Randima <dilinarandima333@gmail.com>
2025-12-22 10:45:00 +0530

    created app component

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div className="app">
+            <Navbar />
+            <Hero />
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
+// Update 44 - 1687923371
+// Update 46 - 977247899
+// Update 53 - 1603788250
+// Update 69 - 1936051703
+// Update 71 - 523860957
+// Update 73 - 1010600926
+// Update 77 - 556805509
+// Update 82 - 213602224
+// Update 46 - 387452973
+// Update 48 - 1120112459
+// Update 51 - 1840863554
+// Update 54 - 1188635042
+// Update 58 - 584053895
+// Update 86 - 399150051
+// Update 89 - 764121434
